d4a944be6b9e221e879ca49e185bac466428f56b
sako-is <isazadasaid@gmail.com>
2022-09-25 11:30:29 -0400

    a few updates

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -77,4 +77,51 @@ return require('packer').startup(function(use)
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
         require("toggleterm").setup()
     end}
+
+    use "lewpoly/sherbet.nvim"
+    use { 
+      'olivercederborg/poimandres.nvim',
+      config = function()
+        require('poimandres').setup {
+          -- leave this setup function empty for default config
+          -- or refer to the configuration section
+          -- for configuration options
+        }
+      end
+    }
+    use 'Yazeed1s/minimal.nvim'
+
+    use {
+        "olimorris/onedarkpro.nvim", 
+        config = function()
+            require("onedarkpro").setup({
+                dark_theme = "onedark", -- The default dark theme
+                light_theme = "onelight", -- The default light theme
+                colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
+                highlights = {}, -- Override default highlight groups
+                ft_highlights = {}, -- Override default highlight groups for specific filetypes
+                plugins = { -- Override which plugin highlight groups are loaded
+    -- See the Supported Plugins section for a list of available plugins
+                },
+                styles = { -- Choose from "bold,italic,underline"
+                    strings = "NONE", -- Style that is applied to strings.
+                    comments = "NONE", -- Style that is applied to comments
+                    keywords = "NONE", -- Style that is applied to keywords
+                    functions = "NONE", -- Style that is applied to functions
+                    variables = "NONE", -- Style that is applied to variables
+                    virtual_text = "NONE", -- Style that is applied to virtual text
+                },
+		options = {
+      			bold = false, -- Use the colorscheme's opinionated bold styles?
+      			italic = false, -- Use the colorscheme's opinionated italic styles?
+      			underline = false, -- Use the colorscheme's opinionated underline styles?
+      			undercurl = false, -- Use the colorscheme's opinionated undercurl styles?
+      			cursorline = false, -- Use cursorline highlighting?
+      			transparency = false, -- Use a transparent background?
+      			terminal_colors = false, -- Use the colorscheme's colors for Neovim's :terminal?
+      			window_unfocused_color = false, -- When the window is out of focus, change the normal background?
+  		}
+	})
+	end
+}
 end)
